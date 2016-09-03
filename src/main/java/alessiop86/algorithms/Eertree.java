4290e1baf7161c75ba587a51ef45d6985fff452a
alessiop <alessio.piergiacomi@gmail.com>
2016-09-03 11:28:04 +0100

    some cleanigns pre rename

--- src/main/java/alessiop86/algorithms/Eertree.java
+++ src/main/java/alessiop86/algorithms/Eertree.java
@@ -4,37 +4,48 @@ public class Eertree {
 
     private static final int MAXN = 105000;
 
+    int stringLength;
+    char[] s = new char[MAXN]; //why MAXN?
+    Node[] tree = new Node[MAXN]; //why MAXN?
+    int suff; 			// max suffix palindrome
+    //long ans;
+
     public Eertree(String str) {
         s = str.toCharArray();
-        len = str.length();
+        stringLength = str.length();
     }
 
     class Node {
-        int next[] = new int[26]; ;
+        //int next[] = new int[26]; //strictly following the paper I should have used a balanced BST here
+        int outgoingNodesIndexes[] = new int[26]; //strictly following the paper I should have used a balanced BST here.
+                                                 // Each Node can have at most 1 outgoing node for every symbol of the alphabet in my case
         int len;
         int sufflink;
     }
 
     void initTree() {
+        //2 suffixes palindromes until now (number of nodes??? check)
         suff = 2;
+
+        //imaginary string
         tree[1] = new Node();
         tree[1].len = -1;
         tree[1].sufflink = 1;
+
+        //empty string
         tree[2] = new Node();
         tree[2].len = 0;
         tree[2].sufflink = 1;
     }
 
-    int len;
-    char[] s = new char[MAXN]; //why MAXN?
-    Node[] tree = new Node[MAXN]; //why MAXN?
-    int suff; 			// max suffix palindrome
-    long ans;
+
 
     public boolean addLetter(int pos) {
         int cur = suff;
         int curlen = 0;
-        int let = s[pos] - 'a';
+        int let = s[pos] - 'a'; //to transform in char
+        //char letter = s[pos];
+
 
         while (true) {
             curlen = tree[cur].len;
@@ -42,8 +53,8 @@ public class Eertree {
                 break;
             cur = tree[cur].sufflink;
         }
-        if (tree[cur].next[let] != 0) {
-            suff = tree[cur].next[let];
+        if (tree[cur].outgoingNodesIndexes[let] != 0) {//if (tree[cur].next[let] != 0) {
+            suff = tree[cur].outgoingNodesIndexes[let];//  suff = tree[cur].next[let];
             return false;
         }
 
@@ -81,16 +92,15 @@ public class Eertree {
 
         eertree.build();
 
-        System.out.println(eertree.ans);
+        //System.out.println(eertree.ans);
     }
 
 
 
     private void build() {
         initTree();
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < stringLength; i++) {
             addLetter(i);
-//            ans += tree[suff].num;
         }
     }
 }
